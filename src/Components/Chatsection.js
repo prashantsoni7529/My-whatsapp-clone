b2d5238441970d7e69626ec569296c345a512f53
Prashant Soni <prashantsonikota@gmail.com>
2021-04-23 14:28:19 +0530

    Completed UI part of whatsApp WEB

--- src/Components/Chatsection.js
+++ src/Components/Chatsection.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Avatar, IconButton } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
@@ -6,16 +6,35 @@ import SentimentVerySatisfiedOutlinedIcon from '@material-ui/icons/SentimentVery
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MicIcon from '@material-ui/icons/Mic';
 
+
 import './chatsection.css';
 
 function ChatSection() {
+
+    const [seed,setSeed]=useState('');
+    const[input,setInput]=useState('');
+
+    const sendMessage=(e)=>{
+       e.preventDefault();
+       console.log("Your typed",input);
+       setInput('');
+    }
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()*5000));
+    }, []);
+
     return (
         <div className="ChatSection">
 
            <div className="ChatSection_Header">
-           <IconButton>
-                    <Avatar>P</Avatar>
-                </IconButton>
+             <IconButton>
+                  <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+             </IconButton>
+             <div className="ChatSection_Header_info">
+                 <h3>Room Name</h3>
+                 <p>Last seen at...</p>
+             </div>
 
 
                 <div className="ChatSection_Header_right">
@@ -28,13 +47,40 @@ function ChatSection() {
                     </IconButton>
                 </div>
             </div> 
-            <div className="ChatSection_Chatbox">
-                {/* here are our chatting messages code come */}
+            <div className="chat_body">
+                <p className={`chat_message  ${false && "chat_receiver"}`}>
+                    <span className="chat_name">Aditya Pratap</span>
+                    Aur Bhai ki haal chal?
+                    <span className="chat_timestemp"> 11:50pm</span>
+
+                </p>
+                <p className={`chat_message  ${true && "chat_receiver"}`}>
+                    <span className="chat_name">Aditya Pratap</span>
+                    Aur Bhai ki haal chal?
+                    <span className="chat_timestemp"> 11:50pm</span>
+
+                </p>
+                <p className={`chat_message  ${false && "chat_receiver"}`}>
+                    <span className="chat_name">Aditya Pratap</span>
+                    Aur Bhai ki haal chal?
+                    <span className="chat_timestemp"> 11:50pm</span>
+
+                </p>
+                <p className={`chat_message  ${true && "chat_receiver"}`}>
+                    <span className="chat_name">Aditya Pratap</span>
+                    Aur Bhai ki haal chal?
+                    <span className="chat_timestemp"> 11:50pm</span>
+
+                </p>
+               
             </div>
-            <div className="ChatSection_Footer">
+            <div className="chat_footer">
                 <SentimentVerySatisfiedOutlinedIcon/>
                 <AttachFileIcon/>
-                <input type="text" placeholder="Type a message"/>
+                <form>
+                    <input    type="text" value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Type a message"/>
+                    <button type="submit" onClick={sendMessage}  > Send a Message</button>
+                </form>
                 <MicIcon/>
             
             </div>
--- src/Components/SidebarChat.js
+++ src/Components/SidebarChat.js
@@ -1,5 +1,6 @@
 import React,{useEffect,useState} from 'react'
 import { Avatar } from '@material-ui/core';
+import ChatSection from './Chatsection';
 
 function SidebarChat({addnewchat}) {
 
@@ -15,10 +16,12 @@ function SidebarChat({addnewchat}) {
         setSeed(Math.floor(Math.random()*5000));
     }, [])
 
-    
+    // const handleChatRoom=()=>{
+    //     <ChatSection  />
+    // }
 
     return  !addnewchat?(
-        <div className="Sidebarchats">
+        <div className="Sidebarchats" >
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className="chatroominfo">
                 <h3>Room Name</h3>
